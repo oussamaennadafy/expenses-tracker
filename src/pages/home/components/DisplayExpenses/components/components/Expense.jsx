95ac3197c9061d8bd6257d65c646064bbbd04c26
oussama <oussama.ennadafy@gmail.com>
2023-03-10 14:52:03 +0100

    fix single expense component

--- src/pages/home/components/DisplayExpenses/components/components/Expense.jsx
+++ src/pages/home/components/DisplayExpenses/components/components/Expense.jsx
@@ -1,10 +1,25 @@
 function Expense({ title, amount, date }) {
+  date = date.split("-");
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   return (
     <div className="flex justify-between gap-3 p-3 items-center bg-gray-600 rounded border border-gray-600">
       <div className="bg-gray-800 border-2 p-3 border-gray-300 rounded-md flex flex-col justify-center items-center">
-        <p>December</p>
-        <p className="text-sm text-gray-500">2020</p>
-        <p className="text-2xl font-semibold">16</p>
+        <p>{months[parseInt(date[1]) - 1]}</p>
+        <p className="text-sm text-gray-500">{date[0]}</p>
+        <p className="text-2xl font-semibold">{date[2]}</p>
       </div>
       <p className="flex-1 font-semibold">{title}</p>
       <div className="border border-gray-800 px-4 py-1 bg-violet-600 rounded-md">
