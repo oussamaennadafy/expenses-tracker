72b98d4f8531425e69e7c5813ba553eaa8fde55c
oussama <oussama.ennadafy@gmail.com>
2023-03-10 15:18:03 +0100

    fix style of expense component

--- src/pages/home/components/DisplayExpenses/components/components/Expense.jsx
+++ src/pages/home/components/DisplayExpenses/components/components/Expense.jsx
@@ -16,7 +16,7 @@ function Expense({ title, amount, date }) {
   ];
   return (
     <div className="flex justify-between gap-3 p-3 items-center bg-gray-600 rounded border border-gray-600">
-      <div className="bg-gray-800 border-2 p-3 border-gray-300 rounded-md flex flex-col justify-center items-center">
+      <div className="bg-gray-800 border-2 p-3 w-24 border-gray-300 rounded-md flex flex-col justify-center items-center">
         <p>{months[parseInt(date[1]) - 1]}</p>
         <p className="text-sm text-gray-500">{date[0]}</p>
         <p className="text-2xl font-semibold">{date[2]}</p>
